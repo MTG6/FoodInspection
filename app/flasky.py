2b7b112ffe787864e79bce947a6d4c14677800fa
Matthew Gallagher <matthewgallagher@dhcp-10-105-133-156.wireless.northwestern.private>
2018-01-25 15:57:18 -0600

    Practicing flasky

--- app/flasky.py
+++ app/flasky.py
@@ -6,7 +6,18 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 	return 'This is the homepage'
-	
+
+@app.route('/melons')
+def melons():
+	return 'My favorite melons are: Watermelon, Cantelope, HoneyDew, and MountainDew'	
+
+@app.route('/melons/<melonname>')
+def meloninfo(melonname):
+	if melonname.lower() not in ('watermelon', 'cantelope', 'honeydew', 'mountaindew'):
+		res = '%s is not a melon!!' % melonname
+	else:
+		res= '<h2> %s is a very fancy melon<h2>' % melonname
+	return res
 	
 if __name__ == "__main__":
 	app.run(debug=True)
