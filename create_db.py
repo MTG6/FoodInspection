3870b4610951fbd79153db80172806ca1bdb496b
Matthew Gallagher <matthewgallagher@dhcp-10-105-159-61.wireless.northwestern.private>
2018-03-14 15:08:20 -0500

    updated git ignore and print sql

--- create_db.py
+++ create_db.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
 	uri = config.SQLALCHEMY_DATABASE_URI
 	conn = sqlalchemy.engine.create_engine(uri)
 	df_f.to_sql("CleanInspections",conn, if_exists="replace")
-	#print(pd.read_sql_query("select * from CleanInspections limit 5;",conn))
+	print(pd.read_sql_query("select * from CleanInspections limit 5;",conn))
 	
 	#Pickle
 	tm.train_model(pd.read_sql_query("select * from CleanInspections",conn))
