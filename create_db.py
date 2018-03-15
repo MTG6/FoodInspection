2d38e5eeb4588f9659b7e14770b78e5f7ad3b57d
Matthew Gallagher <matthewgallagher@matthews-mbp.lan>
2018-03-14 20:43:02 -0500

    Clean createdb

--- create_db.py
+++ create_db.py
@@ -11,32 +11,18 @@ from app.config import SQLALCHEMY_DATABASE_URI
 
 if __name__ == "__main__":
 	
-	#Set up SQLAlchemy
-	#app = Flask(__name__)
-	#app.config.from_envvar('MSIA_SETTINGS', silent=True)
-	#db = SQLAlchemy(app)
 	
-	#Create sqlite DB
-	#conn = sqlite3.connect("FoodInspect.db")
-	#engine = create_engine('mysql+pymysql:///FoodInspect.db')
-
-	#Load data drame
+	# Import and clean data drame
 	df = ld.load_data()
 	df_f = fc.feature_creation(df)
 	
 	
-	#Try to create DB
-	#engine = create_engine('sqlite:////Users/matthewgallagher/MSiA/Winter_2018/MSIA_423/FoodInspection/FoodInspect.db')
-	#conn = engine.connect()
-	#df_f.to_sql("Inspections",conn, if_exists="replace")
-	#print(pd.read_sql_query("select * from Inspections limit 5;", conn).columns)
-	
-	#AWS CONNECTION
-	#uri ='mysql+pymysql://mtg6:p&&ssF4!L@foodinspection-db.c9hebod1wl2a.us-west-2.rds.amazonaws.com:3306/foodinspectiondatabase' 
+	# Load data into AWS connection
+	# uri ='mysql+pymysql://mtg6:p&&ssF4!L@foodinspection-db.c9hebod1wl2a.us-west-2.rds.amazonaws.com:3306/foodinspectiondatabase' 
 	uri = SQLALCHEMY_DATABASE_URI
 	conn = sqlalchemy.engine.create_engine(uri)
 	df_f.to_sql("CleanInspections",conn, if_exists="replace")
 	print(pd.read_sql_query("select * from CleanInspections limit 5;",conn))
 	
-	#Pickle
+	# Pickle model
 	tm.train_model(pd.read_sql_query("select * from CleanInspections",conn))
