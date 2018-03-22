c2166603c4215c1a1618663a32b64835f9a18e38
Matthew Gallagher <matthewgallagher@vpn-165-124-165-8.vpn.northwestern.edu>
2018-03-22 13:06:38 -0500

    update tests 3

--- test/test_features.py
+++ test/test_features.py
@@ -2,16 +2,17 @@ import pytest
 import pandas as pd
 import numpy as np
 import sys
-#sys.path.append("../")
+sys.path.append("../")
 from data import feature_creation as fc
 from data import load_data as ld
 
 
 def test_pass_fail():
-	data = np.array(['pass','fail'])
+	data = np.array(['Pass','Fail'])
 	testdata = pd.Series(data)
-	assert  fc.pass_fail(testdata)[1] == 1
+	assert  fc.pass_fail(testdata)[1] == 0
 	
 def test_fc():
-	data = ld.load_data().iloc[1:2]
-	assert len(fc.feature_creation(data).columns) == 17
+	data = ld.load_data()
+	tdata = fc.feature_creation(data)
+	assert len(tdata.columns) == 74
